d1699cb03ea9f5d9520ef31ca71059ccc6a6dd51
vjcvictor <victorj.cuero@gmail.com>
2022-11-16 19:28:37 -0500

    Adelanto Cambio de Clave

--- src/app/modulos/seguridad/cambio-clave/cambio-clave.component.ts
+++ src/app/modulos/seguridad/cambio-clave/cambio-clave.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import * as cryptoJS from "crypto-js";
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 
 @Component({
   selector: 'app-cambio-clave',
   templateUrl: './cambio-clave.component.html',
   styleUrls: ['./cambio-clave.component.css']
 })
-export class CambioClaveComponent implements OnInit {
 
-  constructor() { }
+export class IdentificacionComponent implements OnInit {
+  fgValidador: FormGroup = this.fb.group({
+    'clave-anterior': ['', [Validators.required]],
+    'clave-nueva-1': ['',[Validators.required]],
+    'clave-nueva-2': ['',[Validators.required]]
+   });
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    this.servicioSeguridad.Identificar(usuario, claveCifrada).subscribe((datos:any) => {
+      this.servicioSeguridad.AlmacenarSesion(datos);
+      this.router.navigate(["/inicio"]);
+    }, (error: any) => {
+      // KO
+      alert("Datos Inválidos")
+    })
+  }
+
+
 }
+
